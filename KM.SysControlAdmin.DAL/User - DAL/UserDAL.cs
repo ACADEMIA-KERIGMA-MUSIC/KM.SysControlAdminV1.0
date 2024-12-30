0532efb9e39677c3ee08d9e749c9be5080a94d2a
Hector Garcia <hg425823@gmail.com>
2024-12-30 00:56:50 -0600

    LOGICA Y VISTA PARA GUARDAR USUARIO

---
+++ KM.SysControlAdmin.DAL/User - DAL/UserDAL.cs
@@ -0,0 +1,72 @@
+﻿#region REFERENCIAS
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+// Referencias Necesarias Para El Correcto Funcionamiento
+using System.Security.Cryptography;
+using System.Text;
+using KM.SysControlAdmin.EN.User___EN;
+using Microsoft.EntityFrameworkCore;
+
+
+#endregion
+
+namespace KM.SysControlAdmin.DAL.User___DAL
+{
+    public class UserDAL
+    {
+        #region METODO PARA ENCRIPTAR
+        // Metodo Para Encriptar Via MD5 El Password
+        private static void EncryptMD5(User user)
+        {
+            using (var md5 = MD5.Create())
+            {
+                var result = md5.ComputeHash(Encoding.ASCII.GetBytes(user.Password));
+                var encryptedStr = "";
+                for (int i = 0; i < result.Length; i++)
+                {
+                    encryptedStr += result[i].ToString("x2").ToLower();
+                }
+                user.Password = encryptedStr;
+            }
+        }
+        #endregion
+
+        #region METODO PARA VALIDAR EXISTENCIA DEL USUARIO
+        // Metodo Para Validar La Existencia o No De Un Usuario
+        private static async Task<bool> ExistsUser(User user, ContextDB dbContext)
+        {
+            bool result = false;
+            var userLoginExists = await dbContext.User.FirstOrDefaultAsync(u => u.Email == user.Email && u.Id != user.Id);
+            if (userLoginExists != null && userLoginExists.Id > 0 && userLoginExists.Email == user.Email)
+                result = true;
+
+            return result;
+        }
+        #endregion
+
+        #region METODO PARA GUARDAR
+        // Metodo Para Guardar Un Nuevo Registro En La Base De Datos
+        public static async Task<int> CreateAsync(User user)
+        {
+            int result = 0;
+            using (var dbContext = new ContextDB())
+            {
+                bool UserExists = await ExistsUser(user, dbContext);
+                if (UserExists == false)
+                {
+                    user.DateCreated = DateTime.Now;
+                    user.DateModification = DateTime.Now;
+                    EncryptMD5(user);
+                    dbContext.User.Add(user);
+                    result = await dbContext.SaveChangesAsync();
+                }
+                else
+                    throw new Exception("Usuario Ya Existente, Vuelve a Intentarlo.");
+            }
+            return result;
+        }
+        #endregion
+    }
+}
---
+++ KM.SysControlAdmin.WebApp/Controllers/User - Controller/UserController.cs
@@ -0,0 +1,65 @@
+﻿#region REFERENCIAS
+// Referencias Necesarias Para El Correcto Funcionamiento
+using KM.SysControlAdmin.BL.Role___BL;
+using KM.SysControlAdmin.BL.User___BL;
+using KM.SysControlAdmin.EN.User___EN;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+#endregion
+
+namespace KM.SysControlAdmin.WebApp.Controllers.User___Controller
+{
+    public class UserController : Controller
+    {
+        // Creamos Las Instancias Para Acceder a Los Metodos
+        UserBL userBL = new UserBL();
+        RoleBL roleBL = new RoleBL();
+
+        #region METODO PARA GUARDAR
+        // Accion Que Muestra El Formulario
+        //[Authorize(Roles = "Desarrollador, Administrador, Secretario/a")]
+        public async Task<IActionResult> Create()
+        {
+            var roles = await roleBL.GetAllAsync();
+            ViewBag.Roles = roles;
+            return View();
+        }
+
+        // Accion Que Recibe Los Datos y Los Envia a La Base De Datos
+        //[Authorize(Roles = "Desarrollador, Administrador, Secretario/a")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(User user, IFormFile imagen)
+        {
+            try
+            {
+                // Mapeo de img a ArrayByte
+                if (imagen != null && imagen.Length > 0)
+                {
+                    byte[] imagenData = null!;
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        await imagen.CopyToAsync(memoryStream);
+                        imagenData = memoryStream.ToArray();
+                    }
+
+                    user.ImageData = imagenData; // Asigna el array de bytes al campo de la imagen en tu modelo Membership
+                }
+                user.DateCreated = DateTime.Now;
+                user.DateModification = DateTime.Now;
+                int result = await userBL.CreateAsync(user);
+                TempData["SuccessMessageCreate"] = "Usuario Agregado Exitosamente";
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception e)
+            {
+                ViewBag.Error = e.Message;
+                ViewBag.Roles = await roleBL.GetAllAsync();
+                return View(user);
+            }
+        }
+        #endregion
+    }
+}
