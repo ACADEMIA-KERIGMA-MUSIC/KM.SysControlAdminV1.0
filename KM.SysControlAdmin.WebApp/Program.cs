34e15a8c067cf1181d4deb67590500743ab874a2
Hector Garcia <hg425823@gmail.com>
2025-01-05 00:42:37 -0600

    INSTALACION Y CONFIGURACION DE ROTATIVA

--- KM.SysControlAdmin.WebApp/Program.cs
+++ KM.SysControlAdmin.WebApp/Program.cs
@@ -35,4 +35,8 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+// Configuración para Rotativa(Reportes vista HTML a PDF)
+IWebHostEnvironment env = app.Environment;
+Rotativa.AspNetCore.RotativaConfiguration.Setup(env.WebRootPath, "../wwwroot/lib/Rotativa");
+
 app.Run();
