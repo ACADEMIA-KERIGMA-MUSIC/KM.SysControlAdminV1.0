3e7ade4104b6c1b3b5a9c3a57fb343a135dfa335
Hector Garcia <hg425823@gmail.com>
2025-01-05 00:18:52 -0600

    INSERCION DE LOGICAS POR MOSTRAR TODOS ,POR ID. ADEMAS DE MODIFICAR Y ELIMINAR

--- KM.SysControlAdmin.BL/Trainer - BL/TrainerBL.cs
+++ KM.SysControlAdmin.BL/Trainer - BL/TrainerBL.cs
@@ -21,5 +21,45 @@ namespace KM.SysControlAdmin.BL.Trainer___BL
             return await TrainerDAL.CreateAsync(trainer);
         }
         #endregion
+
+        #region METODO PARA MOSTRAR
+        // Metodo Para Mostrar Una Lista De Registros
+        public async Task<List<Trainer>> GetAllAsync()
+        {
+            return await TrainerDAL.GetAllAsync();
+        }
+        #endregion
+
+        #region METODO PARA MOSTRAR POR ID
+        // Metodo Para Mostrar Un Registro Especifico Bajo Un Id
+        public async Task<Trainer> GetByIdAsync(Trainer trainer)
+        {
+            return await TrainerDAL.GetByIdAsync(trainer);
+        }
+        #endregion
+
+        #region METODO PARA BUSCAR
+        // Metodo Para Buscar Registros Existentes
+        public async Task<List<Trainer>> SearchAsync(Trainer trainer)
+        {
+            return await TrainerDAL.SearchAsync(trainer);
+        }
+        #endregion
+
+        #region METODO PARA MODIFICAR
+        // Metodo Para Guardar Un Nuevo Registro
+        public async Task<int> UpdateAsync(Trainer trainer)
+        {
+            return await TrainerDAL.UpdateAsync(trainer);
+        }
+        #endregion
+
+        #region METODO PARA ELIMINAR
+        // Metodo Para Eliminar Un Registro Existente En La Base De Datos
+        public async Task<int> DeleteAsync(Trainer trainer)
+        {
+            return await TrainerDAL.DeleteAsync(trainer);
+        }
+        #endregion
     }
 }
--- KM.SysControlAdmin.WebApp/Controllers/Trainer - Controller/TrainerController.cs
+++ KM.SysControlAdmin.WebApp/Controllers/Trainer - Controller/TrainerController.cs
@@ -78,5 +78,163 @@ namespace KM.SysControlAdmin.WebApp.Controllers.Trainer___Controller
             }
         }
         #endregion
+
+        #region METODO PARA MOSTRAR INDEX
+        // Accion Para Mostrar La Vista Index
+        [Authorize(Roles = "Desarrollador, Administrador, Secretario/a")]
+        public async Task<IActionResult> Index(Trainer trainer = null!)
+        {
+            if (trainer == null)
+                trainer = new Trainer();
+
+            var trainers = await trainerBL.SearchAsync(trainer);
+            return View(trainers);
+        }
+        #endregion
+
+        #region METODO PARA MODIFICAR
+        // Acción que muestra la vista de modificar
+        [Authorize(Roles = "Desarrollador, Administrador, Secretario/a")]
+        public async Task<IActionResult> Edit(int id)
+        {
+            try
+            {
+                Trainer trainer = await trainerBL.GetByIdAsync(new Trainer { Id = id });
+                if (trainer == null)
+                {
+                    return NotFound();
+                }
+                // Convertir el array de bytes en imagen para mostrar en la vista
+                if (trainer.ImageData != null && trainer.ImageData.Length > 0)
+                {
+                    ViewBag.ImageUrl = Convert.ToBase64String(trainer.ImageData);
+                }
+                return View(trainer);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return View(); // Devolver la vista sin ningún objeto Membership
+            }
+        }
+
+        // Acción que recibe los datos del formulario para ser enviados a la base de datos
+        [Authorize(Roles = "Desarrollador, Administrador, Secretario/a")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Trainer trainer, IFormFile imagen)
+        {
+            try
+            {
+                if (id != trainer.Id)
+                {
+                    return BadRequest();
+                }
+                if (imagen != null && imagen.Length > 0) // Verificar si se ha subido una nueva imagen
+                {
+                    byte[] imagenData = null!;
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        await imagen.CopyToAsync(memoryStream);
+                        imagenData = memoryStream.ToArray();
+                    }
+                    trainer.ImageData = imagenData; // Asignar el array de bytes de la nueva imagen a la entidad Membership
+                }
+                else
+                {
+                    // Si no se proporciona una nueva imagen, se conserva la imagen existente
+                    Trainer existingTrainer = await trainerBL.GetByIdAsync(new Trainer { Id = id });
+                    trainer.ImageData = existingTrainer.ImageData;
+                }
+                trainer.DateModification = DateTime.Now;
+                int result = await trainerBL.UpdateAsync(trainer);
+                TempData["SuccessMessageUpdate"] = "Instructor/Docente Modificado Exitosamente";
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return View(trainer); // Devolver la vista con el objeto Membership para que el usuario pueda corregir los datos
+            }
+        }
+        #endregion
+
+        #region METODO PARA MOSTRAR DETALLES
+        // Accion Que Muestra El Detalle De Un Registro
+        [Authorize(Roles = "Desarrollador, Administrador, Secretario/a")]
+        public async Task<IActionResult> Details(int id)
+        {
+            try
+            {
+                Trainer trainer = await trainerBL.GetByIdAsync(new Trainer { Id = id });
+                if (trainer == null)
+                {
+                    return NotFound();
+                }
+                // Convertir el array de bytes en imagen para mostrar en la vista
+                if (trainer.ImageData != null && trainer.ImageData.Length > 0)
+                {
+                    ViewBag.ImageUrl = Convert.ToBase64String(trainer.ImageData);
+                }
+                return View(trainer); // Retornamos los Detalles a La Vista
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return View(); // Devolver la vista sin ningún objeto Membership
+            }
+        }
+        #endregion
+
+        #region METODO PARA ELIMINAR
+        // Accion Que Muestra La Vista De Eliminar
+        [Authorize(Roles = "Desarrollador, Administrador, Secretario/a")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                Trainer trainer = await trainerBL.GetByIdAsync(new Trainer { Id = id });
+
+                if (trainer == null)
+                {
+                    return NotFound();
+                }
+                // Convertir el array de bytes en imagen para mostrar en la vista
+                if (trainer.ImageData != null && trainer.ImageData.Length > 0)
+                {
+                    ViewBag.ImageUrl = Convert.ToBase64String(trainer.ImageData);
+                }
+                return View(trainer);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return View(); // Devolver la vista sin ningún objeto Membership
+            }
+        }
+
+        // Accion Que Recibe Los Datos Del Formulario Para Ser Enviados a La BD
+        [Authorize(Roles = "Desarrollador, Administrador, Secretario/a")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id, Trainer trainer)
+        {
+            try
+            {
+                Trainer trainerDB = await trainerBL.GetByIdAsync(trainer);
+                int result = await trainerBL.DeleteAsync(trainerDB);
+                TempData["SuccessMessageDelete"] = "Instructor/Docente Eliminado Exitosamente";
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                var trainerDB = await trainerBL.GetByIdAsync(trainer);
+                if (trainerDB == null)
+                    trainerDB = new Trainer();
+                return View(trainerDB);
+            }
+        }
+        #endregion
     }
 }
