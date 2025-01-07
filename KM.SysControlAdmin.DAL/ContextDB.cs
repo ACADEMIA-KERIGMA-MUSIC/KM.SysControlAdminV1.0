d3e334c488fb88d519916b10e962358a6cbbfa9e
Hector Garcia <hg425823@gmail.com>
2025-01-07 13:29:01 -0600

    ACTUALIZACION DE CONTEXTDB

--- KM.SysControlAdmin.DAL/ContextDB.cs
+++ KM.SysControlAdmin.DAL/ContextDB.cs
@@ -11,6 +11,7 @@ using KM.SysControlAdmin.EN.Role___EN;
 using KM.SysControlAdmin.EN.User___EN;
 using KM.SysControlAdmin.EN.Schedule___EN;
 using KM.SysControlAdmin.EN.Trainer___EN;
+using KM.SysControlAdmin.EN.Course___EN;
 
 
 #endregion
@@ -25,6 +26,7 @@ namespace KM.SysControlAdmin.DAL
         public DbSet<User> User { get; set; }
         public DbSet<Schedule> Schedule { get; set; }
         public DbSet<Trainer> Trainer { get; set; }
+        public DbSet<Course> Course { get; set; }
         #endregion
 
         #region STRING DE CONEXION
