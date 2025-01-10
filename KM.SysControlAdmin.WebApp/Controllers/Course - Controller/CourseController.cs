3e463c76f4a228641f0252386be5777e0ef58bf4
Hector Garcia <hg425823@gmail.com>
2025-01-10 11:03:08 -0600

    INSERCION DE LOGICA PARA MODIFICAR Y MOSTRAR DATOS PARA COURSE

--- KM.SysControlAdmin.WebApp/Controllers/Course - Controller/CourseController.cs
+++ KM.SysControlAdmin.WebApp/Controllers/Course - Controller/CourseController.cs
@@ -55,5 +55,73 @@ namespace KM.SysControlAdmin.WebApp.Controllers.Course___Controller
             }
         }
         #endregion
+
+        #region METODO PARA MOSTRAR INDEX
+        // Accion Para Mostrar La Vista Index
+        [Authorize(Roles = "Desarrollador, Administrador, Secretario/a")]
+        public async Task<IActionResult> Index(Course course = null!)
+        {
+            if (course == null)
+                course = new Course();
+
+            var courses = await courseBL.SearchIncludeScheduleAndTrainerAsync(course);
+            var trainer = await trainerBL.GetAllAsync();
+            var schedule = await scheduleBL.GetAllAsync();
+
+            ViewBag.Trainers = trainer;
+            ViewBag.Schedule = schedule;
+            return View(courses);
+        }
+        #endregion
+
+        #region METODO PARA MODIFICAR
+        // Acción que muestra la vista de modificar
+        [Authorize(Roles = "Desarrollador, Administrador, Secretario/a")]
+        public async Task<IActionResult> Edit(int id)
+        {
+            try
+            {
+                Course course = await courseBL.GetByIdAsync(new Course { Id = id });
+                if (course == null)
+                {
+                    return NotFound();
+                }
+                ViewBag.Trainers = await trainerBL.GetAllAsync();
+                ViewBag.Schedule = await scheduleBL.GetAllAsync();
+                return View(course);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return View();
+            }
+        }
+
+        // Acción que recibe los datos del formulario para ser enviados a la base de datos
+        [Authorize(Roles = "Desarrollador, Administrador, Secretario/a")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Course course)
+        {
+            try
+            {
+                if (id != course.Id)
+                {
+                    return BadRequest();
+                }
+                course.DateModification = DateTime.Now;
+                int result = await courseBL.UpdateAsync(course);
+                TempData["SuccessMessageUpdate"] = "Curso Modificado Exitosamente";
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                ViewBag.Trainers = await trainerBL.GetAllAsync();
+                ViewBag.Schedule = await scheduleBL.GetAllAsync();
+                return View(course);
+            }
+        }
+        #endregion
     }
 }
