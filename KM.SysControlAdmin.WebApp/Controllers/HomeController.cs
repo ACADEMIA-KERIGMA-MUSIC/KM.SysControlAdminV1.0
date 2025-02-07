8d1f5690a5342935ae7d1c19b2bede18cbd456b5
Hector Garcia <hg425823@gmail.com>
2025-02-07 07:02:20 -0600

    PRIMER AVANCE DEL METODO DASHBOARD DEL HOMECONTROLLER

--- KM.SysControlAdmin.WebApp/Controllers/HomeController.cs
+++ KM.SysControlAdmin.WebApp/Controllers/HomeController.cs
@@ -1,3 +1,6 @@
+using KM.SysControlAdmin.BL.Course___BL;
+using KM.SysControlAdmin.BL.Student___BL;
+using KM.SysControlAdmin.BL.Trainer___BL;
 using KM.SysControlAdmin.WebApp.Models;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
@@ -9,6 +12,11 @@ namespace KM.SysControlAdmin.WebApp.Controllers
     [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme, Roles = "Desarrollador, Administrador, Secretario/a, Instructor/Docente, Alumno/a")]
     public class HomeController : Controller
     {
+        // Creamos Una Instancia Para Acceder a Los Metodos
+        StudentBL studentBL = new StudentBL();
+        CourseBL courseBL = new CourseBL();
+        TrainerBL trainerBL = new TrainerBL();
+
         [Authorize(Roles = "Desarrollador, Administrador, Secretario/a, Instructor/Docente, Alumno/a")]
         public IActionResult Index()
         {
@@ -16,8 +24,26 @@ namespace KM.SysControlAdmin.WebApp.Controllers
         }
 
         [Authorize(Roles = "Desarrollador, Administrador")]
-        public IActionResult Dashboard()
+        public async Task<IActionResult> Dashboard()
         {
+            int totalAlumnos = await studentBL.GetTotalCountAsync(); // Total de estudiantes
+            int totalAlumnosActivos = await studentBL.GetActiveStudentsCountAsync(); // Total de estudiantes activos
+            int totalCursosActivos = await courseBL.GetActiveCourseCountAsync();
+            int totalInstructores = await trainerBL.GetTotalCountAsync();
+            int totalBecados = await studentBL.GetScholarshipStudentsCountAsync();
+            int totalExternos = await studentBL.GetExternalStudentsCountAsync();
+            int totalCursos = await courseBL.GetCourseCountAsync();
+            int totalCursosInactivos = await courseBL.GetInactiveCourseCountAsync();
+
+            ViewData["TotalAlumnos"] = totalAlumnos;
+            ViewData["TotalAlumnosActivos"] = totalAlumnosActivos;
+            ViewData["TotalCursosActivos"] = totalCursosActivos;
+            ViewData["TotalInstructores"] = totalInstructores;
+            ViewData["TotalBecados"] = totalBecados;
+            ViewData["TotalExternos"] = totalExternos;
+            ViewData["TotalCursos"] = totalCursos;
+            ViewData["TotalCursosInactivos"] = totalCursosInactivos;
+
             return View();
         }
     }
