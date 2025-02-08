2c5317487fe55d8c9809ea13c922359ba2cdf652
Hector Garcia <hg425823@gmail.com>
2025-02-08 10:32:18 -0600

    ACTUALIZACION DE VISTA DASHBOARD CON NUEVO GRAFICO E INSERCION DE CONFIG. EN PROGRAM PARA MANEJO DE ERRORES

--- KM.SysControlAdmin.WebApp/Program.cs
+++ KM.SysControlAdmin.WebApp/Program.cs
@@ -19,7 +19,7 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseStatusCodePagesWithRedirects("/Home/CustomStatusCode?code={0}"); // Maneja códigos de error específicos
     app.UseHsts();
 }
 
